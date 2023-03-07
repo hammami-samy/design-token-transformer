ae6a70d4f81fa41f9681ba627df9cb2a892bc1f6
hammami-samy <hammami-samy@users.noreply.github.com>
2023-03-07 15:55:15 +0000

    Tokens updated

--- build/ios/StyleDictionarySize.h
+++ build/ios/StyleDictionarySize.h
@@ -3,15 +3,27 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Jul 2022 19:49:49 GMT
+// Generated on Tue, 07 Mar 2023 15:55:15 GMT
 
 
 #import <Foundation/Foundation.h>
 
 
-extern float const TypographyBodyH3FontWeight;
-extern float const TypographyBodyH4StrikeThroughFontWeight;
-extern float const TypographyBodyItalicFontWeight;
-extern float const TypographyBodyExtraBoldCondensedItalicFontWeight;
-extern float const TypographyBodyMediumExtendedItalicFontWeight;
-extern float const TypographyBodySuperFontWeight;
+extern float const TypographyLargeScreenDisplayXlargeFontWeight;
+extern float const TypographyLargeScreenDisplayLargeFontWeight;
+extern float const TypographyLargeScreenDisplayMediumFontWeight;
+extern float const TypographyLargeScreenDisplaySmallFontWeight;
+extern float const TypographyLargeScreenHeadingFontWeight;
+extern float const TypographyLargeScreenSubheadingFontWeight;
+extern float const TypographyLargeScreenButtonFontWeight;
+extern float const TypographyLargeScreenBodyFontWeight;
+extern float const TypographyLargeScreenCaptionFontWeight;
+extern float const TypographySmallScreenDisplayXlargeFontWeight;
+extern float const TypographySmallScreenDisplayLargeFontWeight;
+extern float const TypographySmallScreenDisplayMediumFontWeight;
+extern float const TypographySmallScreenDisplaySmallFontWeight;
+extern float const TypographySmallScreenHeadingFontWeight;
+extern float const TypographySmallScreenSubheadingFontWeight;
+extern float const TypographySmallScreenButtonFontWeight;
+extern float const TypographySmallScreenBodyFontWeight;
+extern float const TypographySmallScreenCaptionFontWeight;
